722ec58ae03bec867328beca7e04d899ea2cf67f
unknown <aamir.khan2478@gmail.com>
2022-05-12 00:42:58 +0500

    updated code server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 //Cors
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors());
 
 //Database Connection
 require("./Database/conn");
