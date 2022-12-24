85db16bda906c903a1ad74f8da44df308c56c79a
aamirkhan2478 <aamir.khan2478@gmail.com>
2022-12-24 22:54:59 +0500

    This is now just backend repo not a clint repo

--- client/src/Layouts/LoginLayout/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-
-const LoginLayout = () => {
-  return <div><Outlet/></div>;
-};
-
-export default LoginLayout;
--- client/src/Layouts/ReusableComponents/Protected.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {Outlet,Navigate} from 'react-router-dom'
-
-export const DashboardProtected =()=>{
-    return localStorage.getItem('authToken')?<Outlet/>:<Navigate to='/login/app'/> 
-}
-export const LoginProtected =()=>{
-    return !localStorage.getItem('authToken')?<Outlet/>:<Navigate to='/app/dashboard'/> 
-}
